bf833577681715ea28e2751d17611e164bfb292d
RogerLeitel8 <roger.leite@etec.sp.gov.br>
2019-08-19 16:42:25 -0300

    adicionar aula06

---
+++ first.py
@@ -0,0 +1,19 @@
+def receber_valor(separar_valor):
+    for i in range(4):
+        numero = []
+        digito = int(input("Digite um número: "))
+        numero.append(digito)
+    return separar_valor()
+def separar_valor():
+    if (digito % 2) == 0:
+        par.append(digito)
+    else:
+        impar.append(digito)
+
+print(receber_valor())
+
+
+
+
+
+
---
+++ number.py
@@ -0,0 +1,4 @@
+numero = input('Informe o numero do celular')
+if len(numero) == 9 and numero[0] == '9':
+    print('Numero Valido')
+else: print('Numero Invalido')
